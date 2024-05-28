8f004553da86bf261779ecc7d3a6130d4112ec33
mustungajames <mutsungajames@gmail.com>
2024-05-28 13:35:58 +0200

    Branche heritage

--- main.py
+++ main.py
@@ -10,3 +10,36 @@ class Enseignat(conges):
 
         def decrire(self, nom=None, prenom=None, nbre_heur_annee=None):
             print(nom,prenom,nbre_heur_annee)
+    """HERITAGE"""
+#Deuxième class enseignat chercheur qui herite la class Enseingant
+
+class Enseignat_chercheur(Enseignat):
+        def __init__(self,heur_complem_cherch):
+            self.heur_complem_cherch=heur_complem_cherch
+#Fonction qui calcule le salaire de chercheur
+        def calculer_salire_cherch(self,heur_complem_cherch):
+            if heur_complem_cherch>192:
+                resultat=(2000*12)+heur_complem_cherch*40
+            elif heur_complem_cherch<=192:
+                resultat=(2000*12)
+            return resultat
+
+#Troisième class enseignant vacateur qui herite  de la class  Enseignat
+class Enseignant_vacataire(Enseignat):
+        def __init__(self,heure_vacataire):
+            self.heure_vacataire=heure_vacataire
+#Fonction qui calcule le salaire de vacateur
+        def calculer_salaire_vacataire(self,heure_vacataire):
+            resultat=heure_vacataire*40
+            return resultat
+#Quatrième class de doctorant qui herite de la class Enseingant
+class Enseignant_doctorat(Enseignat):
+        def __init__(self,heure_doctorat):
+            self.heure_doctorat=heure_doctorat
+#Fonction qui calcule  le salaire de docteur
+        def calculer_salaire_doctorat(self,heure_doctorat):
+            if heure_doctorat<=96:
+                resultat =heure_doctorat * 30
+            elif heure_doctorat>96:
+                resultat = 96 * 30
+            return resultat
