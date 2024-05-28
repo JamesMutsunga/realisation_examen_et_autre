2bbd526e624fdf3a2aaedcf480f51f6c266a1828
mustungajames <mutsungajames@gmail.com>
2024-05-28 13:52:18 +0200

    Branche polymorphisme

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 class Enseignat():
 #Premier class enseignat et le constructeur par defaut
-        def __int__(self, nom=None, prenom=None, nbre_heur_annee=None):
-            self.nom = nom
-            self.prenom = prenom
-            self.nbre_heur_annee = nbre_heur_annee
-
-        def decrire(self, nom=None, prenom=None, nbre_heur_annee=None):
-            print(nom,prenom,nbre_heur_annee)
+    def __int__(self, nom=None, prenom=None, nbre_heur_annee=None):
+        self.nom = nom
+        self.prenom = prenom
+        self.nbre_heur_annee = nbre_heur_annee
+    def decrire(self):
+        print(f"Cet enseignant est {self.nom}, {self.prenom},{self.nbre_heur_annee}")
+    """POLYMORPHISME"""
+class Assistant(Enseignat):
+    def decrire(self, nom=None, prenom=None, nbre_heur_annee=None):
+        print(f"Celui ci est un assistant {nom}, {prenom},{nbre_heur_annee}" )
