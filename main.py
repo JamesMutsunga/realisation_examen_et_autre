8ba8dd8e0acb544adde64b1ef116bd92e30498be
mustungajames <mutsungajames@gmail.com>
2024-05-28 18:47:40 +0200

    encapscilation

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 class Enseignat():
 #Premier class enseignat et le constructeur par defaut
         def __int__(self, nom=None, prenom=None, nbre_heur_annee=None):
-            self.nom = nom
-            self.prenom = prenom
-            self.nbre_heur_annee = nbre_heur_annee
+            self.__nom = nom
+            self.__prenom = prenom
+            self.__nbre_heur_annee = nbre_heur_annee
 
         def decrire(self, nom=None, prenom=None, nbre_heur_annee=None):
             print(nom,prenom,nbre_heur_annee)
