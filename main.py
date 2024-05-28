889af9d94b1e2f2ea3f6735ce625c5496db6186a
mustungajames <mutsungajames@gmail.com>
2024-05-28 14:00:11 +0200

    Branche encapsulation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 class Enseignat():
 #Premier class enseignat et le constructeur par defaut
     def __int__(self, nom=None, prenom=None, nbre_heur_annee=None):
-        self.nom = nom
-        self.prenom = prenom
-        self.nbre_heur_annee = nbre_heur_annee
-        self.salaire=100
+        """Encapsulation"""
+        self.__nom = nom
+        self.__prenom = prenom
+        self.__nbre_heur_annee = nbre_heur_annee
+        self.__salaire=100
     def decrire(self):
         print(f"Cet enseignant est {self.nom}, {self.prenom},{self.nbre_heur_annee}")
     def payer(self,montant):
